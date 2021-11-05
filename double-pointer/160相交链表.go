d7d6f48b7568f7196cdc5a1c69a11f69e65fffb9
levy <15958732798@163.com>
2021-11-05 14:19:29 +0800

    LeetCode160

---
+++ double-pointer/160相交链表.go
@@ -0,0 +1,31 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//判断节点，不是节点值
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	curA, curB := headA, headB
+	for curA != curB {
+		if curA == nil {
+			curA = headB
+		} else {
+			curA = curA.Next
+		}
+		if curB == nil {
+			curB = headA
+		} else {
+			curB = curB.Next
+		}
+	}
+	return curA
+}
