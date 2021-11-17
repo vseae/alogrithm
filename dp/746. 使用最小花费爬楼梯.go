9eb92034a95c6912e8a3f5b1e80b9f3424e94fc5
levy <15958732798@163.com>
2021-11-17 15:37:17 +0800

    LeetCode746

---
+++ dp/746. 使用最小花费爬楼梯.go
@@ -0,0 +1,24 @@
+package main
+
+func minCostClimbingStairs(cost []int) int {
+	n := len(cost)
+	dp := make([]int, len(cost))
+	//dp[i]=min(dp[i-1],dp[i-2)+cost[i]
+	dp[0] = cost[0]
+	dp[1] = cost[1]
+	for i := 2; i < n; i++ {
+		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+	}
+	return min(dp[n-1], dp[n-2])
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+//func main() {
+//
+//}
