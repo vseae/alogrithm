09c44a41e500b1c7db9447b84c9cf49b33b760f7
levy <15958732798@163.com>
2021-11-17 22:33:22 +0800

    LeetCode343

---
+++ dp/343. 整数拆分.go
@@ -0,0 +1,20 @@
+package main
+
+func integerBreak(n int) int {
+	dp := make([]int, n+1)
+	//dp[i] = max(dp[i],max(dp[i-1]*j,(i-j)*j)
+	dp[1] = 1
+	dp[2] = 1
+	for i := 3; i < n+1; i++ {
+		for j := 1; j < i-1; j++ {
+			dp[i] = max(dp[i], max(dp[i-j]*j, (i-j)*j))
+		}
+	}
+	return dp[n]
+}
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
