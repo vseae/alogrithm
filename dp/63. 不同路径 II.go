ad54fd422162e20472150837c8af91eb7ecc4712
levy <15958732798@163.com>
2021-11-17 22:33:11 +0800

    LeetCode63

---
+++ dp/63. 不同路径 II.go
@@ -0,0 +1,30 @@
+package main
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m := len(obstacleGrid)    //纵坐标
+	n := len(obstacleGrid[0]) //横坐标
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	//dp[i][j]=dp[i-1][j]+dp[i][j-1]
+	//dp[i][j]=dp[i-1][j]
+	//dp[i][j]=dp[i][j-1]
+	for i := 0; i < m && obstacleGrid[i][0] == 0; i++ {
+		dp[i][0] = 1
+	}
+	for j := 0; j < n && obstacleGrid[0][j] == 0; j++ {
+		dp[0][j] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] != 1 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
+func main() {
+
+}
