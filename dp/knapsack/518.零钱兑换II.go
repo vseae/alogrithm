f85c1e1c48b734da00e2c363a908a8071f9db08d
levy <15958732798@163.com>
2021-11-30 15:19:45 +0800

    LeetCode518

--- dp/knapsack/518.零钱兑换II.go
+++ dp/knapsack/518.零钱兑换II.go
@@ -1,58 +1,21 @@
-package knapsack
-
-/*
-
-给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
-
-请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
-
-假设每一种面额的硬币有无限个。
-
-题目数据保证结果符合 32 位带符号整数。
-
-
-
-示例 1：
-
-输入：amount = 5, coins = [1, 2, 5]
-输出：4
-解释：有四种方式可以凑成总金额：
-5=5
-5=2+2+1
-5=2+1+1+1
-5=1+1+1+1+1
-示例 2：
-
-输入：amount = 3, coins = [2]
-输出：0
-解释：只用面额 2 的硬币不能凑成总金额 3 。
-示例 3：
-
-输入：amount = 10, coins = [10]
-输出：1
-
-
-*/
+package main
 
 func change(amount int, coins []int) int {
-	//1.确定状态
-	//两个状态；i，前i个物品；j，背包容量
-	dp := make([][]int, len(coins)+1)
-	for i := range dp {
-		dp[i] = make([]int, amount+1)
-		dp[i][0] = 1
-	}
+	//1.确定dp数组的含义
+	//dp[j]
 	n := len(coins)
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= amount; j++ {
-			//选择-放入
-			if j-coins[i-1] >= 0 {
-				dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]
-			} else { //选择-不放入
-				dp[i][j] = dp[i-1][j]
-			}
+	dp := make([]int, amount+1)
+	//2.确定递推顺序
+	//dp[j]=max(dp[j],dp[j-conis[i]]+1)
 
+	//3.确定边界
+	//dp[0]=1
+	dp[0] = 1
+	//4.遍历顺序 先遍历物品再遍历容量
+	for i := 0; i < n; i++ {
+		for j := coins[i]; j <= amount; j++ {
+			dp[j] += dp[j-coins[i]]
 		}
 	}
-	return dp[n][amount]
+	return dp[amount]
 }
