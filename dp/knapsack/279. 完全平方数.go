530da8782bbbfc284b045432c154f87de55d21fa
levy <15958732798@163.com>
2021-12-16 20:47:08 +0800

    LeetCode279

---
+++ dp/knapsack/279. 完全平方数.go
@@ -0,0 +1,25 @@
+package main
+
+import "math"
+
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 0
+	for j := 1; j <= n; j++ {
+		dp[j] = math.MaxInt32
+	}
+	//dp[j] += dp[j-i*i]
+	for i := 1; i <= n; i++ {
+		for j := i * i; j <= n; j++ {
+			dp[j] = min(dp[j], dp[j-i*i]+1)
+		}
+	}
+	return dp[n]
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
