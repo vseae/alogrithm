6ff3c6b83d9fbc207d05637883382336d3564c58
levy <15958732798@163.com>
2021-12-16 17:28:34 +0800

    LeetCode322

--- dp/knapsack/322零钱兑换.go
+++ dp/knapsack/322零钱兑换.go
@@ -1,74 +1,33 @@
 package main
 
-import "fmt"
+import "math"
 
-/*
-给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
-
-计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
-
-你可以认为每种硬币的数量是无限的。
-
-
-
-示例 1：
-
-输入：coins = [1, 2, 5], amount = 11
-输出：3
-解释：11 = 5 + 5 + 1
-示例 2：
-
-输入：coins = [2], amount = 3
-输出：-1
-示例 3：
-
-输入：coins = [1], amount = 0
-输出：0
-示例 4：
-
-输入：coins = [1], amount = 1
-输出：1
-示例 5：
-
-输入：coins = [1], amount = 2
-输出：2
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/coin-change
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-*/
 func coinChange(coins []int, amount int) int {
-	//确定状态：物品数量和背包容量,i和j
-	dp := make([][]int, len(coins)+1)
-	for i := range dp {
-		dp[i] = make([]int, amount+1)
-	}
+	//确定dp数组:dp[j] 凑足j所需钱币最少个数为dp[j]
+	dp := make([]int, amount+1)
+	//确定递推公式 dp[j] = min(dp[j-coins[i]]+1,dp[j])
+	//确定边界
+	dp[0] = 0
 	for j := 1; j <= amount; j++ {
-		dp[0][j] = amount + 1
+		dp[j] = math.MaxInt32
 	}
-	n := len(coins)
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= amount; j++ {
-			if j-coins[i-1] >= 0 {
-				dp[i][j] = min(dp[i-1][j], dp[i][j-coins[i-1]]+1)
-			} else {
-				dp[i][j] = dp[i-1][j]
+	//遍历
+	for i := 0; i < len(coins); i++ {
+		for j := coins[i]; j <= amount; j++ {
+			if dp[j-coins[i]] != math.MaxInt32 {
+				dp[j] = min(dp[j], dp[j-coins[i]]+1)
 			}
 		}
 	}
-	if dp[n][amount] == amount+1 {
+	if dp[amount] == math.MaxInt32 {
 		return -1
-	} else {
-		return dp[n][amount]
 	}
-}
+	return dp[amount]
 
-//func main() {
-//	fmt.Println(coinChange([]int{1, 2, 5}, 11))
-//}
+}
 func min(x, y int) int {
-	if x < y {
-		return x
+	if x > y {
+		return y
 	}
-	return y
+	return x
 }
