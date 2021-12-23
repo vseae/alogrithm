fb52a9e69d10cf4b6ce1aa2c7404aaa74bbe12b3
levy <15958732798@163.com>
2021-12-23 16:05:01 +0800

    LeetCode309

---
+++ dp/309. 最佳买卖股票时机含冷冻期.go
@@ -0,0 +1,28 @@
+package main
+
+func maxProfit(prices []int) int {
+	//dp[i][0] dp[i][1]
+	//dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+	//dp[i][1] = max(dp[i-1][1],dp[i-2][0]-price[i])
+	n := len(prices)
+	if n < 2 {
+		return 0
+	}
+	dp := make([][2]int, n)
+	dp[0][0] = -prices[0]
+	dp[0][1] = 0
+	dp[1][0] = max(dp[0][0], dp[0][1]-prices[1]) //第一天持有（昨天买的，今天买的）
+	dp[1][1] = max(dp[0][0]+prices[1], dp[0][1]) //第一天不持有（今天卖，昨天就卖）
+	for i := 2; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-2][1]-prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
+	}
+	return dp[n-1][1]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
