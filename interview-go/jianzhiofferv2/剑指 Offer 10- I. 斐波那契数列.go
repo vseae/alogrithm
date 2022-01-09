385c671fce75675bc6c8d7cee21838c785df965e
levy <15958732798@163.com>
2022-01-09 20:16:03 +0800

    interview

--- interview-go/jianzhiofferv2/剑指 Offer 10- I. 斐波那契数列.go
+++ interview-go/jianzhiofferv2/剑指 Offer 10- I. 斐波那契数列.go
@@ -5,9 +5,10 @@ func fib(n int) int {
 		return n
 	}
 	a, b, c := 0, 1, 0
-	for i := 1; i < n; i++ {
-		c = a + b
-		a, b = b, c
+	for i := 2; i <= n; i++ {
+		c = (a + b) % 1000000007
+		a = b
+		b = c
 	}
-	return c
+	return b
 }
