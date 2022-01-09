30085a80bdcfe77714bfebec6be65091ac6c4244
levy <15958732798@163.com>
2022-01-09 17:47:05 +0800

    LeetCode300

---
+++ dp/300. 最长递增子序列.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func lengthOfLIS(nums []int) int {
+	//dp[i]：dp[i]表示i之前包括i的最长上升子序列的长度
+	//if nums[i]>nums[i-1] dp[i]=dp[i-1]+1
+	//if nums[i]<=nums[i-1] dp[i]=dp[i-1]
+	n := len(nums)
+	dp := make([]int, n)
+	for i, _ := range nums {
+		dp[i] = 1
+		fmt.Println(dp[i])
+	}
+	if len(nums) <= 1 {
+		return 1
+	}
+	result := 0
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		result = max(result, dp[i])
+	}
+
+	return result
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+func main() {
+	nums := []int{0}
+	fmt.Println(lengthOfLIS(nums))
+}
