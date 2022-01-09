a2aeea4bf9ed453b5cb01c39764b3318a78ff5dc
levy <15958732798@163.com>
2022-01-09 19:33:31 +0800

    LeetCode27

---
+++ array/27. 移除元素.go
@@ -0,0 +1,13 @@
+package array
+
+func removeElement(nums []int, val int) int {
+	n := len(nums)
+	res := 0
+	for i := 0; i < n; i++ {
+		if nums[i] != val {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+	return res
+}
