a36d359ecb19b0e060848b44eba571e5d89442aa
levy <15958732798@163.com>
2022-01-10 16:24:13 +0800

    update

---
+++ interview-go/jianzhiofferv2/剑指 Offer 11. 旋转数组的最小数字.go
@@ -0,0 +1,23 @@
+package jianzhiofferv2
+
+func minArray(numbers []int) int {
+
+	if len(numbers) == 0 {
+		return 0
+	}
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+	fast := 1
+	slow := 0
+	n := len(numbers)
+	for slow < fast && fast < n {
+		if numbers[fast] >= numbers[slow] {
+			slow++
+			fast++
+		} else {
+			return numbers[fast]
+		}
+	}
+	return numbers[0]
+}
