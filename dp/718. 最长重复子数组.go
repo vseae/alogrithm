a7e1d921f76e44adc3406aab0a513f9d3b874468
levy <15958732798@163.com>
2022-01-10 15:03:50 +0800

    718

---
+++ dp/718. 最长重复子数组.go
@@ -0,0 +1,26 @@
+package main
+
+func findLength(nums1 []int, nums2 []int) int {
+	m := len(nums1)
+	n := len(nums2)
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	res := 0
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			}
+			if dp[i][j] > res {
+				res = dp[i][j]
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+
+}
