1fc19669ee1522e00b8c0b86efd98f0c3878c8ab
levy <15958732798@163.com>
2022-01-10 15:26:26 +0800

    update

---
+++ interview-go/jianzhiofferv2/剑指 Offer 04. 二维数组中的查找.go
@@ -0,0 +1,22 @@
+package jianzhiofferv2
+
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	n := len(matrix)
+	m := len(matrix[0])
+	i := n - 1
+	j := 0
+	for i >= 0 && j <= m-1 {
+		if matrix[i][j] == target {
+			return true
+		}
+		if matrix[i][j] > target {
+			i--
+		} else if matrix[i][j] < target {
+			j++
+		}
+	}
+	return false
+}
