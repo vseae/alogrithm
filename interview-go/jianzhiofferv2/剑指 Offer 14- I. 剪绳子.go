4ecd0524ffd67e3ad1221afdb9f9022be108c230
levy <15958732798@163.com>
2022-01-11 13:50:27 +0800

    update

---
+++ interview-go/jianzhiofferv2/剑指 Offer 14- I. 剪绳子.go
@@ -0,0 +1,27 @@
+package main
+
+func cuttingRope(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = 1
+		for j := 1; j <= i-1; j++ {
+			n := max(dp[j]*(i-j), j*(i-j))
+			if n > dp[i] {
+				dp[i] = n
+			}
+		}
+	}
+	return dp[n]
+	//dp[j]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+func main() {
+
+}
