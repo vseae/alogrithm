df9e7b3cb26387455c607cc485a48e526ef023ec
levy <15958732798@163.com>
2022-01-11 16:41:13 +0800

    update

--- integer/剑指 Offer 16. 数值的整数次方.go
+++ integer/剑指 Offer 16. 数值的整数次方.go
@@ -1,40 +1,50 @@
 package main
 
-import "fmt"
-
 //快速幂+递归
 func myPow(x float64, n int) float64 {
-	if n >= 0 {
-		return APow(x, n)
-	} else {
-		return BPow(x, n)
+	if x == 0 {
+		return 0
 	}
-
-}
-
-func APow(x float64, n int) float64 {
 	var res float64 = 1
+	if n < 0 {
+		x = 1 / x
+		n = -n
+	}
 	for n > 0 {
 		if n&1 == 1 {
-			res = res * x
+			res *= x
 		}
-		x = x * x
+		x *= x
 		n = n >> 1
 	}
 	return res
+
 }
 
-func BPow(x float64, n int) float64 {
-	var res float64 = 1
-	for n < 0 {
-		if n&1 == 1 {
-			res = res / x
-		}
-		x = x * x
-		n = n / 2
-	}
-	return res
-}
-func main() {
-	fmt.Println(myPow(2.10000, -2))
-}
+//
+//func APow(x float64, n int) float64 {
+//	var res float64 = 1
+//	for n > 0 {
+//		if n&1 == 1 {
+//			res = res * x
+//		}
+//		x = x * x
+//		n = n >> 1
+//	}
+//	return res
+//}
+//
+//func BPow(x float64, n int) float64 {
+//	var res float64 = 1
+//	for n < 0 {
+//		if n&1 == 1 {
+//			res = res / x
+//		}
+//		x = x * x
+//		n = n / 2
+//	}
+//	return res
+//}
+//func main() {
+//	fmt.Println(myPow(2.10000, -2))
+//}
