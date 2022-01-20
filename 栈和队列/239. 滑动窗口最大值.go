b10fd8bdd5b8b962993270f6bec86616031636ff
levy <15958732798@163.com>
2022-01-20 14:55:13 +0800

    update

---
+++ 栈和队列/239. 滑动窗口最大值.go
@@ -0,0 +1,55 @@
+package main
+
+type MyQueue struct {
+	queue []int
+}
+
+func NewMyQueue() *MyQueue {
+	return &MyQueue{
+		queue: make([]int, 0),
+	}
+}
+
+func (m *MyQueue) Front() int {
+	return m.queue[0]
+}
+
+func (m *MyQueue) Back() int {
+	return m.queue[len(m.queue)-1]
+}
+
+func (m *MyQueue) Empty() bool {
+	return len(m.queue) == 0
+}
+
+func (m *MyQueue) Push(val int) {
+	for !m.Empty() && val > m.Back() {
+		m.queue = m.queue[:len(m.queue)-1]
+	}
+	m.queue = append(m.queue, val)
+}
+
+func (m *MyQueue) Pop(val int) {
+	if !m.Empty() && val == m.Front() {
+		m.queue = m.queue[1:]
+	}
+}
+func maxSlidingWindow(nums []int, k int) []int {
+	queue := NewMyQueue()
+	n := len(nums)
+	res := make([]int, 0)
+	for i := 0; i < k; i++ {
+		queue.Push(nums[i])
+	}
+	res = append(res, queue.Front())
+	for i := k; i < n; i++ {
+		queue.Pop(nums[i-k])
+		queue.Push(nums[i])
+		res = append(res, queue.Front())
+	}
+	return res
+}
+
+func main() {
+
+}
