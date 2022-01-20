da109042cbf1c03143f2dbf0b83130e7dca13373
levy <15958732798@163.com>
2022-01-20 10:30:59 +0800

    update

---
+++ stack/1047. 删除字符串中的所有相邻重复项.go
@@ -0,0 +1,13 @@
+package main
+
+func removeDuplicates(s string) string {
+	var stack []byte
+	for i := 0; i < len(s); i++ {
+		if len(stack) > 0 && stack[len(stack)-1] == s[i] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return string(stack)
+}
