c0b875c0d2e03d36ea9483e30bde59deedf53be8
levy <15958732798@163.com>
2022-01-28 13:09:38 +0800

    update

--- 链表/双指针-面试题 02.07. 链表相交.go
+++ 链表/双指针-面试题 02.07. 链表相交.go
@@ -1,5 +1,19 @@
 package main
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-
+	curA := headA
+	curB := headB
+	for curA != curB {
+		if curA == nil {
+			curA = headB
+		} else {
+			curA = curA.Next
+		}
+		if curB == nil {
+			curB = headA
+		} else {
+			curB = curB.Next
+		}
+	}
+	return curA
 }
