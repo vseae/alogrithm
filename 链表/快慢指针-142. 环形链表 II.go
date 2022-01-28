f370d52b08512db32240f831f1dc24e95340492c
levy <15958732798@163.com>
2022-01-28 14:31:45 +0800

    update

---
+++ 链表/快慢指针-142. 环形链表 II.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			for slow != head {
+				slow = slow.Next
+				head = head.Next
+			}
+			return head
+		}
+	}
+	return nil
+}
