779706c77a530fc8ccc66ffdf9d252530b71d197
levy <15958732798@163.com>
2022-02-04 14:51:17 +0800

    update

--- 字符串/剑指 Offer 05. 替换空格.go
+++ 字符串/剑指 Offer 05. 替换空格.go
@@ -1,8 +1,34 @@
 package main
 
-import "strings"
-
+// 原地修改
 func replaceSpace(s string) string {
-	res := strings.ReplaceAll(s, " ", "%20")
-	return res
+	b := []byte(s)
+	length := len(b)
+	spaceCount := 0
+	// 计算空格数量
+	for _, v := range b {
+		if v == ' ' {
+			spaceCount++
+		}
+	}
+	// 扩展原有切片
+	resizeCount := spaceCount * 2
+	tmp := make([]byte, resizeCount)
+	b = append(b, tmp...)
+	i := length - 1
+	j := len(b) - 1
+	for i >= 0 {
+		if b[i] != ' ' {
+			b[j] = b[i]
+			i--
+			j--
+		} else {
+			b[j] = '0'
+			b[j-1] = '2'
+			b[j-2] = '%'
+			i--
+			j = j - 3
+		}
+	}
+	return string(b)
 }
