29af76326aeef81136c1d25a4ef7ea40587711ef
levy <15958732798@163.com>
2022-02-22 09:59:40 +0800

    update

--- 复习/哈希表/349. 两个数组的交集.go
+++ 复习/哈希表/349. 两个数组的交集.go
@@ -1,1 +1,22 @@
 package main
+
+func intersection(nums1 []int, nums2 []int) []int {
+	var ans []int
+	exist := make(map[int]bool)
+
+	num1Len := len(nums1)
+	num2Len := len(nums2)
+
+	for i := 0; i < num1Len; i++ {
+		exist[nums1[i]] = true
+	}
+
+	for i := 0; i < num2Len; i++ {
+		if exist[nums2[i]] == true {
+			exist[nums2[i]] = false
+			ans = append(ans, nums2[i])
+		}
+	}
+	return ans
+
+}
